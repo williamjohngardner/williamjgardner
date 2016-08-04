641f1ce2f86ff7aac3dc93d05fde2587f4c06c28
williamjohngardner <bill@williamjohngardner.com>
2016-08-04 19:44:23 -0400

    Initial Commit

---
+++ williamjgardner/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.contrib import admin
+
+from app.views import IndexView
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', IndexView.as_view(), name="index_view")
+]

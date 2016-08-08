81ba01b63f53abca2a7023d80bf1d2f142b740d3
williamjohngardner <bill@williamjohngardner.com>
2016-08-08 18:20:09 -0400

    Developing Single Page Template

--- app/views.py
+++ app/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
+class ModalOneView(TemplateView):
+    template_name = "modal_one_view.html"
--- williamjgardner/urls.py
+++ williamjgardner/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from app.views import IndexView
+from app.views import IndexView, ModalOneView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', IndexView.as_view(), name="index_view")
+    url(r'^$', IndexView.as_view(), name="index_view"),
+    url(r'^modal1/$', ModalOneView.as_view(), name="modal_one_view")
 ]

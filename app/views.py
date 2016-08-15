d6748621d03ce87b0b0d2a0f62d40209ee84d8fb
williamjohngardner <bill@williamjohngardner.com>
2016-08-15 11:17:47 -0400

    Making Page responsive

--- app/views.py
+++ app/views.py
@@ -3,6 +3,3 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "index.html"
-
-class ModalOneView(TemplateView):
-    template_name = "modal_one_view.html"
--- williamjgardner/urls.py
+++ williamjgardner/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from app.views import IndexView, ModalOneView
+from app.views import IndexView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', IndexView.as_view(), name="index_view"),
-    url(r'^modal1/$', ModalOneView.as_view(), name="modal_one_view")
 ]

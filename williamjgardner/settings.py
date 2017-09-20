f7f89a207e537a1d428c2ba99f5a9eba3e9c1f01
williamjohngardner <bill@williamjohngardner.com>
2017-09-20 11:46:28 -0400

    debug off

--- williamjgardner/settings.py
+++ williamjgardner/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'cc#2f+bkfs95v)h_(&a3rly!a1e2^8x0exqw-pvr0vci$qd5zo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
